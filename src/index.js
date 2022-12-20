7ac9f9bc7fdb6d513cc1639fba86e3cd94884d75
EliasSchaut <eschaut@outlook.de>
2022-12-20 10:27:08 +0100

    implement photo take functionality

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@ navigator.mediaDevices.getUserMedia({ video: true })
         alert("Something went wrong while accessing the camera!");
         console.error(error);
     });
+
+function take_snapshot() {
+    const video = document.getElementById('video');
+    const img = document.getElementById('photo');
+    const canvas = document.createElement('canvas');
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0);
+
+    img.src = canvas.toDataURL();
+}
+
+
